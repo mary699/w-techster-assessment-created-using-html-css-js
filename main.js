c3a2e34f8755fd97e581a7ae9a9365b28db9a167
Raymond Abikale <oseafianamarychinwe@gmail.com>
2021-05-19 13:20:45 -0800

    scripting

---
+++ main.js
@@ -0,0 +1,92 @@
+
+function showluke(){
+    var box= document.getElementById("sub-text-one");
+    if (box.style.display =="none" ){
+        box.style.display ="block";
+    } else {
+        box.style.display="none"
+    }
+}
+
+function showc(){
+    var box= document.getElementById("sub-text-two");
+    if (box.style.display =="none" ){
+        box.style.display ="block";
+    } else {
+        box.style.display="none"
+    }
+}
+
+
+function showr(){
+    var box= document.getElementById("sub-text-three");
+    if (box.style.display =="none" ){
+        box.style.display ="block";
+    } else {
+        box.style.display="none"
+    }
+}
+
+function showd(){
+    var box= document.getElementById("sub-text-four");
+    if (box.style.display =="none" ){
+        box.style.display ="block";
+    } else {
+        box.style.display="none"
+    }
+}
+
+function showl(){
+    var box= document.getElementById("sub-text-five");
+    if (box.style.display =="none" ){
+        box.style.display ="block";
+    } else {
+        box.style.display="none"
+    }
+}
+
+function showo(){
+    var box= document.getElementById("sub-text-six");
+    if (box.style.display =="none" ){
+        box.style.display ="block";
+    } else {
+        box.style.display="none"
+    }
+}
+
+function showb(){
+    var box= document.getElementById("sub-text-seven");
+    if (box.style.display =="none" ){
+        box.style.display ="block";
+    } else {
+        box.style.display="none"
+    }
+}
+
+function showrd(){
+    var box= document.getElementById("sub-text-eight");
+    if (box.style.display =="none" ){
+        box.style.display ="block";
+    } else {
+        box.style.display="none"
+    }
+}
+
+function showbd(){
+    var box= document.getElementById("sub-text-nine");
+    if (box.style.display =="none" ){
+        box.style.display ="block";
+    } else {
+        box.style.display="none"
+    }
+}
+
+function showowk(){
+    var box= document.getElementById("sub-text-ten");
+    if (box.style.display =="none" ){
+        box.style.display ="block";
+    } else {
+        box.style.display="none"
+    }
+}
+
